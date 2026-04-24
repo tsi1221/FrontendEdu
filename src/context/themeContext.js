5771bee0acfd5efbeca4336031e458d1f46e3c8e
tsi1221 <tsehayneshbiruh2@gmail.com>
2026-04-24 12:15:51 -0400

    Refactor UI styles, update pages, and add ThemeContext

--- src/context/themeContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const ThemeContext = createContext(null);
-
-export const useTheme = () => useContext(ThemeContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { ThemeProvider } from "./context/ThemeContext";
+// import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
+  // <ThemeProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </ThemeProvider>
+  // </ThemeProvider>
 );
