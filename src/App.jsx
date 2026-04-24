c2b3bfd0f5c3ec7216aa1378ffeac0b03aee2389
tsi1221 <tsehayneshbiruh2@gmail.com>
2026-04-24 08:31:10 -0400

    Add new sidebar components and update auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 import LoginPage from './pages/loginPage'
 import SignupPage from './pages/signinPage'
+
 import SidebarLayout from './componenet/sideBar/SideBar'
+import MainLayout from './Homepage/MainLayout'
+
 import Lab from './pages/student/lab'
 import PracticeHub from './pages/student/practiceHub'
 import TextBook from './pages/student/textbook'
@@ -9,25 +13,64 @@ import Settings from './pages/setting'
 import Chat from './pages/student/chat'
 
 function App() {
-
   return (
-    <div className="w-full h-screen ">
+    <div className="w-full h-screen">
       <Routes>
-        <Route path="/" element={<SidebarLayout><h1 className="text-3xl font-bold">Home Page</h1></SidebarLayout>} />
+
+        {/* Public routes */}
+        <Route path="/" element={<MainLayout />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
 
+        {/* Protected / App layout routes */}
+        <Route
+          path="/chat"
+          element={
+            <SidebarLayout>
+              <Chat />
+            </SidebarLayout>
+          }
+        />
 
-        <Route path="/chat" element={<SidebarLayout><Chat /></SidebarLayout>} />
-        <Route path="/lab" element={<SidebarLayout><Lab /></SidebarLayout>} />
-        <Route path="/practice-hub" element={<SidebarLayout><PracticeHub /></SidebarLayout>} />
-        <Route path="/text-book" element={<SidebarLayout><TextBook /></SidebarLayout>} />
-        <Route path="/settings" element={<SidebarLayout><Settings /></SidebarLayout>} />
+        <Route
+          path="/lab"
+          element={
+            <SidebarLayout>
+              <Lab />
+            </SidebarLayout>
+          }
+        />
 
+        <Route
+          path="/practice-hub"
+          element={
+            <SidebarLayout>
+              <PracticeHub />
+            </SidebarLayout>
+          }
+        />
+
+        <Route
+          path="/text-book"
+          element={
+            <SidebarLayout>
+              <TextBook />
+            </SidebarLayout>
+          }
+        />
+
+        <Route
+          path="/settings"
+          element={
+            <SidebarLayout>
+              <Settings />
+            </SidebarLayout>
+          }
+        />
 
       </Routes>
     </div>
   )
 }
 
-export default App;
+export default App
